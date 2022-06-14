a9587cb751bb52a50adb923488e78493fdb4b60b
ezekiel <ezekiel.nnochiri@turing.com>
2022-06-14 14:33:25 +0100

    changed column variable

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -62,7 +62,7 @@ export const generateUserFileListFromDB = async (tablePayload) => {
 
 export const updateUserWithAuth0Id = async (auth0Payload, tablePayload) => {
     const { connection } = await connectToDb(tablePayload);
-    const updateQuery = `UPDATE ${tablePayload.tableName} SET auth0Id = '${auth0Payload.auth0Id}' WHERE email = '${auth0Payload.email}'`;
+    const updateQuery = `UPDATE ${tablePayload.tableName} SET authO_id = '${auth0Payload.auth0Id}' WHERE email = '${auth0Payload.email}'`;
 
     await connection.query(updateQuery);
 
@@ -72,13 +72,13 @@ export const updateUserWithAuth0Id = async (auth0Payload, tablePayload) => {
 
 export const updateAuth0NullValues = async (tablePayload, accessCredentials) => {
     const { connection } = await connectToDb(tablePayload);
-    const tableCount = `SELECT COUNT(email) as totalTableCount FROM ${tablePayload.tableName} WHERE auth0Id IS NULL`;
+    const tableCount = `SELECT COUNT(email) as totalTableCount FROM ${tablePayload.tableName} WHERE authO_id IS NULL`;
     const [rows] = await connection.query(tableCount);
     const count = rows[0]?.totalTableCount;
     const BATCH_LIMIT = 500;
 
     for (let i = 0; i < count; i += BATCH_LIMIT) {
-        const queryString = `SELECT email FROM ${tablePayload.tableName} WHERE auth0Id IS NULL LIMIT ${BATCH_LIMIT} OFFSET ${i}`;
+        const queryString = `SELECT email FROM ${tablePayload.tableName} WHERE authO_id IS NULL LIMIT ${BATCH_LIMIT} OFFSET ${i}`;
         const [queryResult] = await connection.query(queryString);
 
         for (i = 0; i < queryResult.length; i++) {
@@ -86,7 +86,7 @@ export const updateAuth0NullValues = async (tablePayload, accessCredentials) => 
             if (auth0Response.email && auth0Response.auth0Id) {
                 await updateUserWithAuth0Id(auth0Response, tablePayload);
             }
-            console.log(`Auth0 record not found for email ${queryResult[i].email}`)
+            console.log(`Auth0 record not found for email ${queryResult[i].email}`);
         }
     }
     console.log('Updated user records');
